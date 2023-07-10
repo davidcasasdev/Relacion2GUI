68d459270c23e4a2749e164ab13caa8de3d31436
davidcasasdev <davidecasas@yandex.ru>
2023-07-10 08:59:45 +0200

    ejercicio 2 completo

--- src/gui/VentanaMedias.java
+++ src/gui/VentanaMedias.java
@@ -7,19 +7,24 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import net.miginfocom.swing.MigLayout;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import java.awt.Color;
 import java.awt.Font;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class VentanaMedias extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
+	private JTextField txtNota1;
+	private JTextField txtNota2;
+	private JTextField txtNota3;
+	private JTextField txtNombre;
+	private JLabel lblPromedio;
+	private JLabel lblResultado;
 
 	/**
 	 * Launch the application.
@@ -57,48 +62,74 @@ public class VentanaMedias extends JFrame {
 		lblNewLabel_4.setBackground(Color.BLACK);
 		contentPane.add(lblNewLabel_4, "cell 0 0 6 1,grow");
 		
-		JLabel lblNewLabel = new JLabel("New label");
+		JLabel lblNewLabel = new JLabel("Nombre:");
 		contentPane.add(lblNewLabel, "cell 0 2,alignx trailing");
 		
-		textField_3 = new JTextField();
-		contentPane.add(textField_3, "cell 1 2 5 1,growx");
-		textField_3.setColumns(10);
+		txtNombre = new JTextField();
+		contentPane.add(txtNombre, "cell 1 2 5 1,growx");
+		txtNombre.setColumns(10);
 		
-		JLabel lblNewLabel_1 = new JLabel("New label");
+		JLabel lblNewLabel_1 = new JLabel("Nota 1:");
 		contentPane.add(lblNewLabel_1, "cell 0 4,alignx trailing");
 		
-		textField = new JTextField();
-		contentPane.add(textField, "cell 1 4,growx");
-		textField.setColumns(10);
+		txtNota1 = new JTextField();
+		contentPane.add(txtNota1, "cell 1 4,growx");
+		txtNota1.setColumns(10);
 		
-		JLabel lblNewLabel_2 = new JLabel("New label");
+		JLabel lblNewLabel_2 = new JLabel("Nota 2:");
 		contentPane.add(lblNewLabel_2, "flowx,cell 2 4");
 		
-		JLabel lblNewLabel_3 = new JLabel("New label");
+		JLabel lblNewLabel_3 = new JLabel("Nota 3:");
 		contentPane.add(lblNewLabel_3, "cell 4 4,alignx trailing");
 		
-		textField_2 = new JTextField();
-		contentPane.add(textField_2, "cell 5 4,growx");
-		textField_2.setColumns(10);
+		txtNota3 = new JTextField();
+		contentPane.add(txtNota3, "cell 5 4,growx");
+		txtNota3.setColumns(10);
 		
-		JButton btnNewButton = new JButton("New button");
+		JButton btnNewButton = new JButton("Calcular");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				calcular();
+			}
+		});
 		contentPane.add(btnNewButton, "cell 4 6 2 1");
 		
-		JLabel lblNewLabel_5 = new JLabel("New label");
+		JLabel lblNewLabel_5 = new JLabel("Promedio:");
 		contentPane.add(lblNewLabel_5, "cell 0 8");
 		
-		JLabel lblNewLabel_6 = new JLabel("New label");
-		contentPane.add(lblNewLabel_6, "cell 1 8");
+		lblPromedio = new JLabel("");
+		contentPane.add(lblPromedio, "cell 1 8");
 		
-		JLabel lblNewLabel_7 = new JLabel("New label");
+		JLabel lblNewLabel_7 = new JLabel("Resultado:");
 		contentPane.add(lblNewLabel_7, "cell 0 9");
 		
-		JLabel lblNewLabel_8 = new JLabel("New label");
-		contentPane.add(lblNewLabel_8, "cell 1 9");
+		lblResultado = new JLabel("");
+		contentPane.add(lblResultado, "cell 1 9");
 		
-		textField_1 = new JTextField();
-		contentPane.add(textField_1, "cell 3 4,growx");
-		textField_1.setColumns(10);
+		txtNota2 = new JTextField();
+		contentPane.add(txtNota2, "cell 3 4,growx");
+		txtNota2.setColumns(10);
+	}
+
+	protected void calcular() {
+		try {
+			double nota1 = Double.parseDouble(txtNota1.getText());
+			double nota2 = Double.parseDouble(txtNota2.getText());
+			double nota3 = Double.parseDouble(txtNota3.getText());
+			
+			double media = (nota1+nota2+nota3)/3;
+			
+			this.lblPromedio.setText(String.format("%.2f",media));
+			
+			if (media>=5) {
+				this.lblResultado.setText("Ha aprobado la asignatura");
+			} else {
+				this.lblResultado.setText("Toca recuperar");
+			}
+		} catch(NumberFormatException e) {
+			JOptionPane.showMessageDialog(contentPane, "Debe introducir una nota valida", 
+					"Error en los datos", JOptionPane.ERROR_MESSAGE);
+		}
 	}
 
 }
